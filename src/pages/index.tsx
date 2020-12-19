5c62789c2ca14b46080f4200bf3046d853b87fef
Kazuki Hayashida <>
2020-12-19 18:05:10 +0900

    ファイルを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,23 @@
 import { SampleButton } from "src/components/atoms/common/buttons/SampleButton";
 import { Layout } from "src/components/layout";
+import { Member } from "src/components/organisms/common/Member";
 
 const Home = () => {
-  const onClickSampleButton = () => {
-    alert();
-  };
+  // return (
+  //   <Layout>
+  //     <button
+  //       className="btn-blue"
+  //       onClick={() => {
+  //         window.alert("Hello, World!");
+  //       }}
+  //     >
+  //       Button
+  //     </button>
+  //     <SampleButton onClick={onClickSampleButton}>ボタン</SampleButton>
+  //   </Layout>
+  // );
 
-  return (
-    <Layout>
-      <button
-        className="btn-blue"
-        onClick={() => {
-          window.alert("Hello, World!");
-        }}
-      >
-        Button
-      </button>
-      <SampleButton onClick={onClickSampleButton}>ボタン</SampleButton>
-    </Layout>
-  );
+  return <Member name="ここにname" id="ここにid" state="ここにステータス" />;
 };
 
 // eslint-disable-next-line import/no-default-export
