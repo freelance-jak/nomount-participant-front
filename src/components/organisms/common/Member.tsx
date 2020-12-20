3f70bb9655beda726b9e982f3f82d96ecc04eb20
Kazuki Hayashida <>
2020-12-20 14:20:22 +0900

    いったんコミット

--- src/components/organisms/common/Member.tsx
+++ src/components/organisms/common/Member.tsx
@@ -9,17 +9,16 @@ type Props = {
 export const Member = (props: Props) => {
   const { name, id, isCancel } = props;
   return (
-    <div className="flex flex-wrap justify-center content-center flex-col py-3 px-4 text-primary text-center relative">
-      <div className="absolute right-2 top-2">
-        <SpeechBalloon
-          onClick={() => {
-            return alert();
-          }}
-        />
-      </div>
-
-      <div className="flex flex-wrap justify-center content-center h-24 w-24 border-primary border-solid rounded-full border-2">
-        <span className="text-3xl text-primary">M</span>
+    <div className="flex flex-wrap justify-center content-center flex-col py-3 px-4 text-primary text-center">
+      <div className="flex flex-wrap justify-center content-center h-24 w-24 border-primary border-solid rounded-full border-2 relative">
+        <div className="absolute -right-0 -top-2">
+          <SpeechBalloon
+            onClick={() => {
+              return alert();
+            }}
+          />
+        </div>
+        <span className="text-3xl text-primary">{id.toUpperCase().charAt(0)}</span>
       </div>
 
       <p className="text-primary mt-1">{name}</p>
