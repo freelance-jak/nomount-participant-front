b28f877fe414b5cd62d5bf74abc10b3a7dc05d4b
KAHORI <66770688+kaboko14@users.noreply.github.com>
2020-12-20 13:21:47 +0900

    ユーザーリスト一覧の枠を作りました (#7)
    * ユーザー一覧枠作成
    
    * 'ユーザーリスト一覧のスタイル調整'

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,15 +1,98 @@
 import { SampleButton } from "src/components/atoms/common/buttons/SampleButton";
 import { Layout } from "src/components/layout";
+import { UserListContainer } from "src/components/organisms/common/container/UserListContainer";
 
 const Test = () => {
   const onClickSampleButton = () => {
     alert();
   };
-
+  const UserList = [
+    {
+      name: "みずさん",
+      id: "mizushima",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima1",
+      isCancel: false,
+    },
+    {
+      name: "みずさん",
+      id: "mizushima2",
+      isCancel: false,
+    },
+  ];
   return (
     <Layout>
       <p>ここにコンポーネントをテストで表示する</p>
       <SampleButton onClick={onClickSampleButton}>ボタン</SampleButton>
+      <UserListContainer userList={UserList} />
     </Layout>
   );
 };
